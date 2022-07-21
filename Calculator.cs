2629ea93b0399525b2db5924b0fc956fdd6b77cb
Fabrowser <79614829+Fabrowser@users.noreply.github.com>
2022-07-20 22:06:39 -0300

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array_Soma
+{
+    class Calculator
+    {
+
+        public static int Sum(int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+
+        public static int Sum2(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Array_Soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Sum = Metodo da Classe Calculator  que retorna a soma de qualquer quantidade de valores no Vetor*/
+
+
+            int s1 = Calculator.Sum(new int[] {2,3,4,5,6,7,8,9}); 
+
+            Console.WriteLine(s1);
+
+
+            /* Sum2 = Metodo da Classe Calculator  que retorna a soma de qualquer quantidade de valores no Vetor usando PARAMS
+             (modificador de Parametros*/
+
+            int s2 = Calculator.Sum2(2, 3, 4, 5, 6, 7, 8, 9);
+            
+            Console.WriteLine(s2);
+
+        }
+    }
+}
